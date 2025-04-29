162ce90575887c95feb808d6d0697053eef34d66
MikleSib <Mishatrof21@gmail.com>
2025-04-30 06:23:57 +0700

    fix: api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://рыбный-форум.рф'
-  : 'https://рыбный-форум.рф';
+  ? 'https://рыбный-форум.рф/api'
+  : 'https://рыбный-форум.рф/api';
 
 export default API_URL; 
