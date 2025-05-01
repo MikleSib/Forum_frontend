345e4f6c00c2a6c26761b32be792ed0bc32421e9
MikleSib <Mishatrof21@gmail.com>
2025-05-02 04:27:39 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'Рыболовный форум',
+          description: 'Сообщество рыболовов - форум для обсуждения рыбалки, обмена опытом, поиска рыбных мест и участия в соревнованиях. Присоединяйтесь к нам!',
+          yandexVerification: 'd0bc285c0cf20d50'
+        }
+      },
+      minify: true
+    })
+  ],
+  base: './',
   build: {
     outDir: 'build',
     assetsDir: 'assets',
